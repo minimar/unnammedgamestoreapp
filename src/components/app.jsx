9d7ac82fcd2079459f46b247cf08abedfcdbca3d
IsoVoyd <dubnersamuel@gmail.com>
2021-08-15 02:18:01 -0700

    create electron app

---
+++ src/components/app.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  ReactDOM.render(<h2>Welcome to unnamed game store!</h2>, document.body);
+})
