2160a73af70bf471cb62b30a1b084bced02eae79
minimar <minimarGOO@gmail.com>
2021-09-15 00:47:48 +0100

    Fix Steam banners, now using 'New & Trending' page to avoid javascript

--- steam.py
+++ steam.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://store.steampowered.com/'
+my_url = 'https://store.steampowered.com/explore/new/'
 
 uClient = uReq(my_url)
 page_html = uClient.read()
@@ -9,6 +9,7 @@ uClient.close()
 
 page_soup = soup(page_html, 'html.parser')
 container = page_soup.findAll('a', {'class':'tab_item'})
+container.append(page_soup.findAll('a', {'class':'tab_item.app_impression_tracked'}))
 try:
     count = 0
     for item in container:
