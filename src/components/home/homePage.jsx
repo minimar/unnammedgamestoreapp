1a517b9d9f40bd75d4fb280e73e9e5294a1adf96
minimar <minimarGOO@gmail.com>
2021-10-07 10:46:46 +0100

    Added header and left sidebar, began footer

--- src/components/home/homePage.jsx
+++ src/components/home/homePage.jsx
@@ -1,10 +1,36 @@
 import React from "react"
 
 const HomePage = () => (
-  <div>
-    <button className="buttonTest">
-      Text
-    </button>
+  <div id = "homePage">
+
+  <div id = "header" >
+    <ul className="navbar">
+      <li><a href="here.asp">Here</a></li>
+      <li><a href="be.asp">Be</a></li>
+      <li><a href="will.asp">Will</a></li>
+      <li><a href="options.asp">Options</a></li>
+    </ul>
+  </div>
+  <div id = "leftbar" >
+    <a href="#">Game</a>
+    <a href="#">Genres</a>
+    <a href="#">Will</a>
+    <a href="#">Be</a>
+    <a href="#">Listed</a>
+    <a href="#">Here</a>
+  </div>
+  <div id = "content" >
+    text
+  </div>
+  <div id = "bottomNavbar">
+    <ul className="navbar">
+      <li><a href="interaction.asp">Interaction</a></li>
+      <li><a href="options.asp">Options</a></li>
+      <li><a href="will.asp">Will</a></li>
+      <li><a href="go.asp">Go</a></li>
+      <li><a href="here.asp">Here</a></li>
+    </ul>
+  </div>
   </div>
 )
 
