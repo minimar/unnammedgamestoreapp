e22ccbaa4fed07f238a4510df8d27f5af85fa684
samdubner <dubnersamuel@gmail.com>
2021-10-07 14:18:56 -0700

    use css grid for homepage.css

--- src/components/home/homePage.jsx
+++ src/components/home/homePage.jsx
@@ -1,37 +1,53 @@
-import React from "react"
+import React from "react";
 
 const HomePage = () => (
-  <div id = "homePage">
-
-  <div id = "header" >
-    <ul className="navbar">
-      <li><a href="here.asp">Here</a></li>
-      <li><a href="be.asp">Be</a></li>
-      <li><a href="will.asp">Will</a></li>
-      <li><a href="options.asp">Options</a></li>
-    </ul>
+  <div id="homePage">
+    {/* <div> */}
+    <div id="leftbar">
+      <a href="#">Game</a>
+      <a href="#">Genres</a>
+      <a href="#">Will</a>
+      <a href="#">Be</a>
+      <a href="#">Listed</a>
+      <a href="#">Here</a>
+    </div>
+    <div id="header">
+      <ul className="navbar" id="topNavbar">
+        <li>
+          <a href="here.asp">Here</a>
+        </li>
+        <li>
+          <a href="be.asp">Be</a>
+        </li>
+        <li>
+          <a href="will.asp">Will</a>
+        </li>
+        <li>
+          <a href="options.asp">Options</a>
+        </li>
+      </ul>
+    </div>
+    <div id="content">text</div>
+    <div id="footer">
+      <ul className="navbar" id="bottomNavbar">
+        <li>
+          <a href="interaction.asp">Interaction</a>
+        </li>
+        <li>
+          <a href="options.asp">Options</a>
+        </li>
+        <li>
+          <a href="will.asp">Will</a>
+        </li>
+        <li>
+          <a href="go.asp">Go</a>
+        </li>
+        <li>
+          <a href="here.asp">Here</a>
+        </li>
+      </ul>
+    </div>
   </div>
-  <div id = "leftbar" >
-    <a href="#">Game</a>
-    <a href="#">Genres</a>
-    <a href="#">Will</a>
-    <a href="#">Be</a>
-    <a href="#">Listed</a>
-    <a href="#">Here</a>
-  </div>
-  <div id = "content" >
-    text
-  </div>
-  <div id = "bottomNavbar">
-    <ul className="navbar">
-      <li><a href="interaction.asp">Interaction</a></li>
-      <li><a href="options.asp">Options</a></li>
-      <li><a href="will.asp">Will</a></li>
-      <li><a href="go.asp">Go</a></li>
-      <li><a href="here.asp">Here</a></li>
-    </ul>
-  </div>
-  </div>
-)
+);
 
 export default HomePage;
