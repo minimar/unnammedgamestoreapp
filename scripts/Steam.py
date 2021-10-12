3d8dc6bfaf33d8501dfabf1ef33208c69705856f
minimar <minimarGOO@gmail.com>
2021-10-12 22:46:30 +0100

    Add output as dict

--- scripts/Steam.py
+++ scripts/Steam.py
@@ -1,32 +1,35 @@
 from requests_html import AsyncHTMLSession
+import json
 session = AsyncHTMLSession()
+
+results = {}
 
 async def get_steam():
     r = await session.get('https://store.steampowered.com/explore/new/')
     await r.html.arender()
+    Increment = 0
 
     new_releases = r.html.find('div.tab_content#tab_newreleases_content')
     new_releases_list = new_releases[0].find('a.tab_item')
 
-    # all_new_releases = r.html.find('#tab_allnewreleases_content')
-    # all_new_releases_list = all_new_releases[0].find('.tab_item')
-
-    # all_releases = new_releases_list + all_new_releases_list
-
     for game in new_releases_list:
+        Increment += 1
+        output = {}
         title = game.find('.tab_item_name')
-        print(title[0].text)
+        output["Title"] = title[0].text
 
         try:
             price = game.find('.discount_final_price')
-            print(price[0].text)
+            output["Price"] = price[0].text
         except IndexError:
-            print("Coming Soon")
+            output["Price"] = "Coming Soon"
 
         image = game.find('.tab_item_cap_img')
-        print(image[0].attrs['src'])
+        output["Image"] = image[0].attrs['src']
 
         tags = game.find('.tab_item_top_tags')
-        print(tags[0].text)
+        output["Tags"] = [tags[0].text]
+        results["Result" + str(Increment)] = output
 
 session.run(get_steam)
+print(results)
