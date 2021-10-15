b841e0725ae5d2443414bd705f9ede4d8805b4d2
minimar <minimarGOO@gmail.com>
2021-10-15 01:04:45 +0100

    Script output now in JSON

--- scripts/GOG.py
+++ scripts/GOG.py
@@ -1,18 +1,29 @@
 from requests_html import AsyncHTMLSession
+import json
 session = AsyncHTMLSession()
+
+results = {}
 
 async def get_gog():
   r = await session.get('https://www.gog.com/games?page=1&sort=popularity&tab=new')
   await r.html.arender()
+  Increment = 0
   grid = r.html.find('[ng-show="viewSwitcher.activeView != \'list\'"]', first=False)
   # print(title)
   for game in grid:
+    Increment += 1
+    output = {}
     blockitem = game.find('.product-tile', first=False)
     title = blockitem[0].find('.product-tile__title')
     price = blockitem[0].find('.product-tile__price-discounted')
     img = blockitem[0].find('.product-tile__cover-img')
-    print(title[0].text)
-    print(price[0].text)
-    print(img[0].attrs['lazy-src'])
+    output["Title"] = title[0].text
+    #print(title[0].text)
+    output["Price"] = price[0].text
+    #print(price[0].text)
+    output["Image"] = img[0].attrs['lazy-src']
+    results["Result" + str(Increment)] = output
+    #print(img[0].attrs['lazy-src'])
 
 session.run(get_gog)
+print(results)
--- scripts/Itch.py
+++ scripts/Itch.py
@@ -1,13 +1,18 @@
 from requests_html import AsyncHTMLSession
 session = AsyncHTMLSession()
 
+results = {}
+
 async def get_itch():
     r = await session.get('https://itch.io')
     await r.html.arender()
+    Increment = 0
 
     featuredGames = r.html.find('div.game_cell:not(.index_jam_cell_widget)')
 
     for game in featuredGames:
+        Increment += 1
+        output = {}
         title = game.find('a.title')
         price = game.find('div.meta_tag')
         img = game.find('a.game_thumb')
@@ -16,9 +21,15 @@ async def get_itch():
         tagsText = map(lambda x: x.text[1:], tags)
         tagsString = ' '.join(list(tagsText))
 
-        print(title[0].text)
-        print(price[0].text)
-        print(img[0].attrs['data-background_image'])
-        print(tagsString)
+        output["Title"] = title[0].text
+        #print(title[0].text)
+        output["Price"] = price[0].text
+        #print(price[0].text)
+        output["Image"] = img[0].attrs['data-background_image']
+        #print(img[0].attrs['data-background_image'])
+        output["Tags"] = tagsString
+        #print(tagsString)
+        results["Result" + str(Increment)] = output
 
 session.run(get_itch)
+print(results)
